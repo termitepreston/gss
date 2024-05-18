36544d61dea8f4f307f8ae6d41e6bf20e6ebc036
Alazar Gebremedhin <alazar4018@gmail.com>
2024-05-18 21:55:28 +0300

    Add a `Ticker` event generator.

---
+++ src/gss-server/Ticker.cpp
@@ -0,0 +1,27 @@
+#include "Ticker.hpp"
+
+Ticker::Ticker(net::io_context &ioc, boost::posix_time::seconds interval,
+               const boost::shared_ptr<SharedState> &state)
+    : ioc_{ioc}, interval_{interval}, timer_{ioc, interval}, state_{state} {}
+
+void Ticker::tick(const boost::system::error_code &ec) {
+    spdlog::info("ticking...");
+
+    auto now = boost::posix_time::second_clock::local_time();
+
+    state_->send("ticking.");
+
+    timer_.expires_at(timer_.expires_at() + interval_);
+
+    auto self = shared_from_this();
+
+    timer_.async_wait(
+        [self](const boost::system::error_code &ec) { self->tick(ec); });
+}
+
+void Ticker::run() {
+    auto self = shared_from_this();
+
+    timer_.async_wait(
+        [self](const boost::system::error_code &ec) { self->tick(ec); });
+}
---
+++ src/gss-server/Ticker.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "beast.hpp"
+#include "net.hpp"
+#include "shared_state.hpp"
+#include <boost/optional.hpp>
+#include <boost/smart_ptr.hpp>
+#include <cstdlib>
+#include <memory>
+#include <spdlog/spdlog.h>
+
+class SharedState;
+
+class Ticker : public boost::enable_shared_from_this<Ticker> {
+    net::io_context &ioc_;
+    boost::posix_time::seconds interval_;
+    net::deadline_timer timer_;
+    boost::shared_ptr<SharedState> state_;
+
+    void tick(const boost::system::error_code &ec);
+
+  public:
+    Ticker(net::io_context &ioc, boost::posix_time::seconds interval,
+           const boost::shared_ptr<SharedState> &state);
+    void run();
+};
